671d8c0a3f61034f6c5330b733cf54c184b67d02
Manasa Gulla <manasa.gulla01@gmail.com>
2026-03-06 01:51:06 +0530

    fixxx

--- src/main/java/com/example/pipeline_demo/repository/CalculationRepository.java
+++ src/main/java/com/example/pipeline_demo/repository/CalculationRepository.java
@@ -2,6 +2,8 @@ package com.example.pipeline_demo.repository;
 
 import com.example.pipeline_demo.entity.Calculation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CalculationRepository extends JpaRepository<Calculation, Long> {
 }
